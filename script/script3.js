e4c74f86a37835251170e6636a606074980af483
TarunKoli <amankumar8348@gmail.com>
2022-10-20 23:13:25 +0530

    optimised animation

--- script/script3.js
+++ script/script3.js
@@ -8,34 +8,10 @@ projects.forEach((pj) => {
     prompty.style.animation = "promfy 1s ease-in-out forwards";
     prompt_wrap.style.animation = "wrapfy 1s ease-in-out forwards";
 
-    anime
-      .timeline({ loop: false })
-      .add({
-        targets: ".p_details",
-        translateX: [-750, 0],
-        translateY: [750, 0],
-        opacity: 1,
-        easing: "easeInOutExpo",
-        elasticity: 600,
-        duration: 1500,
-        delay: 200,
-      })
-      .add({
-        targets: ".close",
-        scaleX: [0, 1],
-        scaleY: [0, 1],
-        duration: 100,
-        easing: "spring(1, 1000,5, 0)",
-      });
-
-    anime.timeline({ loop: false }).add({
-      targets: ".p_snap",
-      opacity: 1,
-      easing: "easeInOutExpo",
-      elasticity: 600,
-      duration: 1500,
-      delay: 700,
-    });
+    setTimeout(() => {
+      document.querySelector(".p_snap").style.opacity = 1;
+      document.querySelector(".p_details").style.opacity = 1;
+    }, 1000);
   });
 });
 
