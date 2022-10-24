519619ea645612c47e5ed62ea9b399b339287b97
TarunKoli <amankumar8348@gmail.com>
2022-10-25 01:03:26 +0530

    added projects

---
+++ script/db.js
@@ -0,0 +1,69 @@
+var folder = new Map();
+
+folder.set("zeeq", {
+  media: [
+    { file: "img0", mime: "png" },
+    { file: "img1", mime: "png" },
+    { file: "img2", mime: "png" },
+    { file: "img3", mime: "png" },
+  ],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("edyoumatch", {
+  media: [{ file: "img0", mime: "png" }],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("whelp", {
+  media: [
+    { file: "img0", mime: "png" },
+    { file: "video1", mime: "mp4" },
+  ],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("bike", {
+  media: [
+    { file: "img0", mime: "png" },
+    { file: "img1", mime: "png" },
+    { file: "img2", mime: "png" },
+  ],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("qr", {
+  media: [{ file: "img0", mime: "png" }],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("gallery", {
+  media: [{ file: "img0", mime: "png" }],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("parallax", {
+  media: [{ file: "video0", mime: "mp4" }],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
+
+folder.set("tripify", {
+  media: [{ file: "img0", mime: "png" }],
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat.`,
+});
--- script/script3.js
+++ script/script3.js
@@ -54,3 +54,67 @@ const logo = document.querySelector(".logo");
 logo.addEventListener("click", () => {
   close_nav();
 });
+
+function createCarousel(media) {
+  var slider = document.querySelector(".slider");
+  var next = document.querySelector(".next");
+  var prev = document.querySelector(".prev");
+  var counter = -1;
+
+  next.addEventListener("click", () => {
+    if (counter === media.length - 1) counter = 0;
+    else counter++;
+    slider.scrollTop = 0;
+
+    let temp = [];
+    temp.push(media[counter]);
+    slider.replaceChildren(...temp);
+  });
+
+  prev.addEventListener("click", () => {
+    if (counter === 0) counter = media.length - 1;
+    else counter--;
+    slider.scrollTop = 0;
+
+    let temp = [];
+    temp.push(media[counter]);
+    slider.replaceChildren(...temp);
+  });
+
+  next.click();
+  if (media.length === 1) {
+    next.style.display = "none";
+    prev.style.display = "none";
+    return;
+  } else {
+    next.style.display = "block";
+    prev.style.display = "block";
+  }
+}
+
+function createPrompt(value) {
+  var data = folder.get(value);
+  let media = [];
+
+  data.media.forEach((pj) => {
+    if (pj.mime === "png" || pj.mime === "jpg" || pj.mime === "jpeg") {
+      var img = document.createElement("img");
+      img.src = `./assets/${value}/${pj.file}.${pj.mime}`;
+      media.push(img);
+    } else {
+      var video = document.createElement("video");
+      video.autoplay = true;
+      video.loop = true;
+      video.src = `./assets/${value}/${pj.file}.${pj.mime}`;
+      media.push(video);
+    }
+  });
+
+  createCarousel(media);
+}
+
+projects.forEach((pj) => {
+  pj.addEventListener("click", () => {
+    createPrompt(pj.getAttribute("data-val"));
+  });
+});
