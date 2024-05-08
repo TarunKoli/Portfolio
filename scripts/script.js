84e0820ff84836b0cd5f4b7c84f3fec148b8bab7
Tarun Koli <Tarun.Kumar5@adani.com>
2024-05-08 11:35:49 +0530

    cursor added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,62 @@
-const cusror = document.querySelector('.cursor');
+//https://www.superhi.com/video/smooth-movements-with-javascript
 
-document.addEventListener("mousemove",(e)=>{
-    cusror.style.left = e.pageX + 'px';
-    cusror.style.top = e.pageY + 'px';
+const ball = document.querySelector(".mouse");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let ballX = 0;
+let ballY = 0;
+
+let ballSize = 40;
+
+let speed = 0.05;
+
+
+function animate(){
+  
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+  
+  ballX = ballX + (distX * speed);
+  ballY = ballY + (distY * speed);
+  
+//   ball.style.height = ballSize+"px";
+//   ball.style.width = ballSize+"px";
+
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
+  
+  requestAnimationFrame(animate);
+}
+animate();
+
+document.addEventListener("mousemove", function(event){
+  mouseX = event.pageX;
+  mouseY = event.pageY;
 })
+
+
+let no_cursor_elements = [
+    ".nav-right .links",
+    ".nav-right .sound"
+]
+
+function AddListeners(){
+
+    no_cursor_elements.forEach((elem)=>{
+
+        document.querySelector(elem).addEventListener("mousemove",()=>{
+            ball.style.height = 0+"px";
+            ball.style.width = 0+"px";
+        });
+    
+        document.querySelector(elem).addEventListener("mouseout",()=>{
+            ball.style.height = ballSize+"px";
+            ball.style.width = ballSize+"px";
+        });
+    })
+
+}
+AddListeners();
+
