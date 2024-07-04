e3b7f9bc787f1e9d19740f4432e12ca79ed12c86
Tarun Koli <Tarun.Kumar5@adani.com>
2024-07-04 16:58:01 +0530

    magnetic buttons added

--- scripts/script.js
+++ scripts/script.js
@@ -203,3 +203,49 @@ function animateScroll(){
     requestAnimationFrame(animateScroll);
 }
 animateScroll();
+
+
+function magneticIcons(){
+    const icons = document.querySelectorAll('.nav-left .ic_btn');
+    const maskIcons = document.querySelectorAll('.green-marker .nav-left .ic_btn');
+
+    icons.forEach((icBtn, i)=>{
+
+        icBtn.addEventListener('mousemove', (event) => {
+        var magnetButton = event.currentTarget;
+        var magnetButtonText = magnetButton.querySelector('.ic_content');
+        var maskMagnetButtonText = maskIcons[i].querySelector('.ic_content');
+        var bounding = magnetButton.getBoundingClientRect();
+        var magnetsStrength = 40;
+        var magnetsStrengthText = 20;
+
+
+        let x = (((event.clientX - bounding.left) / magnetButton.offsetWidth) - 0.5) * magnetsStrength;
+        let y = (((event.clientY - bounding.top) / magnetButton.offsetHeight) - 0.5) * magnetsStrength;
+
+        magnetButton.style.transform = 'translate(' + x + 'px,' + y + 'px) rotateZ(0.001deg)';
+        maskIcons[i].style.transform = 'translate(' + x + 'px,' + y + 'px) rotateZ(0.001deg)';
+
+        let textX = (((event.clientX - bounding.left) / magnetButton.offsetWidth) - 0.5) * magnetsStrengthText;
+        let textY = (((event.clientY - bounding.top) / magnetButton.offsetHeight) - 0.5) * magnetsStrengthText;
+
+        magnetButtonText.style.transform = 'translate(' + textX + 'px,' + textY + 'px) rotateZ(0.001deg)';
+        maskMagnetButtonText.style.transform = 'translate(' + textX + 'px,' + textY + 'px) rotateZ(0.001deg)';
+
+        })
+
+        icBtn.addEventListener('mouseleave', (event) => {
+            var magnetButton = event.currentTarget;
+            var magnetButtonText = magnetButton.querySelector('.ic_content');
+            var maskMagnetButtonText = maskIcons[i].querySelector('.ic_content');
+
+            magnetButton.style.transform = 'translate(' + 0 + 'px,' + 0 + 'px) rotateZ(0.001deg)';
+            maskIcons[i].style.transform = 'translate(' + 0 + 'px,' + 0 + 'px) rotateZ(0.001deg)';
+            magnetButtonText.style.transform = 'translate(' + 0 + 'px,' + 0 + 'px) rotateZ(0.001deg)';
+            maskMagnetButtonText.style.transform = 'translate(' + 0 + 'px,' + 0 + 'px) rotateZ(0.001deg)';
+        })
+
+    })
+
+}
+magneticIcons();
